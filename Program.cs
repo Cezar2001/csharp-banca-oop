979742892fcc7ecf5ed581c7625d86065d7f5805
Cezar <cezarlantosgmail.com>
2022-05-17 14:23:05 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace csharp_banca_oop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Banca MiaBanca = new Banca("Banca BCC");
+            Cliente cliente = new Cliente("Mario", "Rossi", "ccccccc", 1200);
+            Console.WriteLine(cliente.ToString());
+
+        }
+    }
+}
